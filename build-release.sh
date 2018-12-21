64895929afd5a965df0f2fa1f730e6e5f8da7333
Fried Hoeben <github@hsac.nl>
2018-12-21 11:47:29 +0100

    Stash instead of reset if commit fails/is aborted

--- build-release.sh
+++ build-release.sh
@@ -105,6 +105,6 @@ cp fitnesse/build/libs/fitnesse-$VERSION-standalone.jar fitnessedotorg/releases/
 
 
 echo "Commit all and push"
-(cd fitnessedotorg && git add $DOWNLOADPAGE $RELEASEDIR releases/$VERSION && git commit -v || { git reset HEAD --hard; die "Not committed, nothing to do."; }; ) \
+(cd fitnessedotorg && git add $DOWNLOADPAGE $RELEASEDIR releases/$VERSION && git commit -v || { git stash; die "Not committed (stashed), nothing to do."; }; ) \
 	&& (cd fitnessedotorg && git push)
 
